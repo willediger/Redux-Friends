7fb6790c7c79a3eb53a2aacf8bb526cd88bbb728
unknown <will.ediger@gmail.com>
2019-04-19 19:09:50 -0500

    added route for /login, and protected-routes for /friends, /friends:id, and /add-friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,34 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return <div className="App" />;
-  }
+import Login from "./components/Login";
+import Friends from "./components/Friends";
+import SingleFriend from "./components/SingleFriend";
+import AddFriendForm from "./components/AddFriendForm";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+          <li>
+            <Link to="/add-friend">Add Friend</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/friends" component={Friends} />
+        <PrivateRoute exact path="/friends/:id" component={SingleFriend} />
+        <PrivateRoute exact path="/add-friend" component={AddFriendForm} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
