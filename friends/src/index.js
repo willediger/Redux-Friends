2cfb8c739c270b849c8db75da6fee47a3b73a11e
unknown <will.ediger@gmail.com>
2019-04-19 19:09:04 -0500

    connected app to redux store, passed in reducer, thunk, logger

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import reducer from "./reducers";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./styles.css";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
